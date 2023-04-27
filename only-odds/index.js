7fc44256bd8c50acd8413ccbc066d0adc2582fa1
Bryan Crawford <bryancrawford14@gmail.com>
2023-04-26 22:07:53 -0400

    created odds only loop

--- only-odds/index.js
+++ only-odds/index.js
@@ -1,4 +1,12 @@
+const array1 = ["2", "4", "6", "8", "10", "11", "12"];
 // first create a array with the a set of number
+let onlyodd = [];
 // create a empty array
-// create a loop that adds odd numbers in the first array to the second array
+for (let i = 0; i < array1.length; i++) {
+  if (array1[i] % 2) {
+    onlyodd.push(array1[i]);
+  }
+  // create a loop that adds odd numbers in the first array to the second array
+}
+console.log(onlyodd);
 // print the second array
