99db64f89f1acf22f2fac5baa49f7fe03a2c374d
Bryan Crawford <bryancrawford14@gmail.com>
2023-04-26 22:12:48 -0400

    created pseudocode for reverse array

---
+++ reverse-array/index.js
@@ -0,0 +1,4 @@
+// create a array with the a set of number
+// Create a empty array for the reverse array
+// Create a loop that takes each number in the first array and adds it the the front of the new array
+// print the new array
