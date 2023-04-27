9710a754e62e02567a87f5da765ed9a4f884ce41
Bryan Crawford <bryancrawford14@gmail.com>
2023-04-26 22:24:57 -0400

    created reverse array loop

--- reverse-array/index.js
+++ reverse-array/index.js
@@ -1,4 +1,10 @@
+const array1 = [1, 2, 3];
 // create a array with the a set of number
+let reverse = [];
 // Create a empty array for the reverse array
-// Create a loop that takes each number in the first array and adds it the the front of the new array
+for (let i = 0; i < array1.length; i++) {
+  reverse.unshift(array1[i]);
+  // Create a for statment that takes each number in the first array and adds it the the front of the new array
+}
+console.log(reverse);
 // print the new array
