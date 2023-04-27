2adb65c9512224e4c47fd45f4164db77a1d30360
Bryan Crawford <bryancrawford14@gmail.com>
2023-04-26 22:35:08 -0400

    created fizzbuzz pseudocode

---
+++ fizzbuzz/index.js
@@ -0,0 +1,5 @@
+// create a loop that prints each number from 1 to 100 on a new line
+// Create a if statment for each multiple of 3 print fizz instead of the number
+// Create a else statment for each multiple of 5 print buzz instead of the number
+// Create a else statment for numbers that are multiples of both 3 and 5 print fizzbuzz instead of the number
+// print the numbers
